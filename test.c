bf64713d768c5a0967fbf69762d1293f1e967ab6
Jacob Adams <tookmund@gmail.com>
2015-02-21 18:36:16 -0500

    try increasing sleep to counterract inefficiency

--- test.c
+++ test.c
@@ -4,9 +4,9 @@ void linefollow() {
 	while (1) {
 		light = analog(5);
 		if (light > 700) { //black line
-			//mav(2,500); //works better with this but inefficient 
+			mav(2,500); //works better with this but inefficient 
 			mav(0,100);
-			msleep(500);
+			msleep(1500);
 			mav(0,500);
 			mav(2,100);
 		}
