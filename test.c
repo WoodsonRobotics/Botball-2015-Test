fb409581098703a3e4529a8f4b5e35b31579e17a
Jacob Adams <tookmund@gmail.com>
2015-02-21 18:08:08 -0500

    Make linefollow better but more ineffcient

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 //follow a black line
-/*
 void linefollow() {
 	int light;
 	while (1) {
 		light = analog(5);
 		if (light > 700) { //black line
+			//mav(2,500); //works better with this but inefficient 
 			mav(0,100);
 			msleep(500);
 			mav(0,500);
@@ -12,11 +12,11 @@ void linefollow() {
 		}
 	}
 }
-*/
-void linefollow();
+
 int main () {
 	mav(0,500);
 	mav(2,500);
+	msleep(1000);
 	linefollow();
 	
 	return 0;
