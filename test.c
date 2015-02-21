6a4f7789d86ddb624e398bfa53402dd68084081f
Jacob Adams <tookmund@gmail.com>
2015-02-21 17:29:21 -0500

    Make comment make more sense

--- test.c
+++ test.c
@@ -3,7 +3,7 @@ void linefollow() {
 	int light;
 	while (1) {
 		light = analog(5);
-		if (light > 700) { //assume black line
+		if (light > 700) { //black line
 			mav(0,100);
 			msleep(500);
 			mav(0,500);
